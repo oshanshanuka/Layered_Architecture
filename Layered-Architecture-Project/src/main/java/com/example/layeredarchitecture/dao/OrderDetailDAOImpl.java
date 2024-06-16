763b5d442236848cdde96345eec7e3a356e18eeb
Oshan shanuka <157113803+oshanshanuka@users.noreply.github.com>
2024-06-16 20:35:31 +0530

    first commit

---
+++ Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAOImpl.java
@@ -0,0 +1,36 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.ItemDTO;
+import com.example.layeredarchitecture.model.OrderDetailDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+public class OrderDetailDAOImpl implements OrderDetailDAO{
+    @Override
+    public boolean saveOrderDetail(String orderId, OrderDetailDTO detail) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement stm = connection.prepareStatement("INSERT INTO `OrderDetails` VALUES (?,?,?,?)");
+
+        stm.setString(1, orderId);
+        stm.setString(2, detail.getItemCode());
+        stm.setInt(3, detail.getQty());
+        stm.setBigDecimal(4, detail.getUnitPrice());
+
+        if (stm.executeUpdate() != 1) {
+            connection.rollback();
+            connection.setAutoCommit(true);
+            return false;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
